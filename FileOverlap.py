6e8733ad7c223fb20be63ccec97a6065e6e6f0b9
Rishabh Srivastav <rishabh@testpress.in>
2018-08-14 19:42:40 +0530

    Print common elements between files of prime and happy numbers

---
+++ FileOverlap.py
@@ -0,0 +1,23 @@
+import urllib.request
+
+
+prime = []
+data1 = urllib.request.urlopen('http://www.practicepython.org/assets/primenumbers.txt')
+for line in data1:
+    line = str(line)
+    line = line.replace("\\n", "")
+    line = line.replace("b'", "")
+    line = line.replace("'", "")
+    prime.append(line)
+happy =[]
+data2 = urllib.request.urlopen("http://www.practicepython.org/assets/happynumbers.txt")
+print(prime)
+for line in data2:
+    line = str(line)
+    line = line.replace("\\n", "")
+    line = line.replace("b'", "")
+    line = line.replace("'", "")
+    happy.append(line)
+print(happy)
+overlap = [i for i in prime if i in happy]
+print(overlap)
